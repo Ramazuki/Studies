47a8b4568abe223afe271c630fe22c2110c92b21
Ramazuki <ramazukigamer@gmail.com>
2023-10-31 15:17:21 +0300

    half of work

--- AI Systems/3/knowledge_base.pl
+++ AI Systems/3/knowledge_base.pl
@@ -1,5 +1,25 @@
 %kingdom(название, запрет на проживание)
-kingdom(wraemor, _).
-kingdom(bortodir,elf).
-kingdom(thelamel,dwarf).
-person(karl,21,human,wraemor).
+kingdom(wraemor, _, francisc).
+kingdom(bortodir,elf, _).
+kingdom(thelamel,dwarf, _).
+
+person(karl,21,elf,wraemor,hunter).
+person(mary,16,human,wraemor,peasant).
+person(francisc,43,human,wraemor,king).
+
+proffesion(X,Y) :-
+    person(X,_,_,_,Y).
+age(X,Y) :-
+    person(X,Y,_,_,_).
+where(X, Y) :-
+    person(X,_,_,Y,_).
+race(X, Y) :-
+    person(X,_,Y,_,_).
+
+is_king(X) :-
+    person(X,_,_,_,Y), Y = king.
+do_live(X, D) :-
+    person(X,_,_,Z,_), Z = D.
+
+can_move(X, D) :-
+    person(X,_,R,_,Y), Y \= king, \+ do_live(X, D), kingdom(D, L, _), R \= L.
--- AI Systems/3/main.py
+++ AI Systems/3/main.py
@@ -1,4 +1,57 @@
 from pyswip import Prolog
+
 prolog = Prolog()
 prolog.consult("knowledge_base.pl")
+
+
+def handle_tf(q):
+    if len(q) > 0:
+        return True
+    return False
+
+
+helpstr = """Для информации по жителям используйте следующие запросы
+Личности - показать все имена
+Королевства - показать все королевства
+Какой [возраст, профессия, раса, королевство] у [имя] - получить значение свойства данной личности
+Кто [младше/старше {число}, расы {название расы (elf, dwarf, human)}, живет в {королевство}, работает {профессия(hunter, knight, king, peasant)}]
+Может [имя] жить в [королевство] - может ли [имя] переехать в [королевство]
+Помощь - вывести эту строку ещё раз
+Выход - завершить работу"""
+print(helpstr)
+
 queue = input()
+while queue != "Выход":
+    if queue.startswith("Какой"):
+        pass
+    elif queue.startswith("Кто"):
+        pass
+    elif queue.startswith("Может"):
+        l = queue.split(" ")
+        name = l[1].lower()
+        kingdom = l[-1].lower()
+        q = list(prolog.query(f'can_move({name}, {kingdom})'))
+        ans = handle_tf(q)
+        if ans:
+            print('Может')
+        else:
+            is_king = handle_tf(list(prolog.query(f"is_king({name})")))
+            if is_king:
+                print('Не может, так как является королём')
+
+    elif queue == "Личности":
+        persons = prolog.query("person(Who,_,_,_,_)")
+        for person in persons:
+            name = str(person.get('Who'))
+            print(name.capitalize())
+    elif queue == "Королевства":
+        kingdoms = prolog.query("kingdom(Name, Law, King)")
+        for kingdom in kingdoms:
+            name = str(kingdom.get('Name'))
+            law = str(kingdom.get('Law'))
+            if law.startswith("_"): law = 'Запретов нет'
+            king = str(kingdom.get('King'))
+            print(f'Королевство {name.capitalize()}\nЗапрет на проживание: {law}\nКороль {king.capitalize()}\n')
+    else:
+        print('Неверный ввод воспользуйтесь "Помощь"')
+    queue = input()
